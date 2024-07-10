44ce5b349b720a10baea4d8086e73f11e4117837
Bryan Rochadel <xadrez90@outlook.com>
2024-07-10 00:59:12 -0300

    upqa

--- qame.py
+++ qame.py
@@ -1,14 +1,21 @@
+from elasticsearch import Elasticsearch
+import pyshark
 import re
 
-# Exemplo de análise de um arquivo de captura
-with open('domestic.pcap', 'r') as file:
-    data = file.read()
+es = Elasticsearch(['http://localhost:9200'])
 
-# Procurar por endereços IP específicos
-ips = re.findall(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', data)
-
-# Procurar por URLs
-urls = re.findall(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', data)
-
-print("Endereços IP encontrados:", ips)
-print("URLs encontradas:", urls)
+cap = pyshark.FileCapture('qame.py')
+for packet in cap:
+    packet_str = str(packet)
+    
+    # Extrair informações relevantes usando RegEx
+    ip = re.search(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', packet_str)
+    url = re.search(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', packet_str)
+    
+    if ip or url:
+        document = {
+            'ip': ip.group(0) if ip else None,
+            'url': url.group(0) if url else None,
+            'raw': packet_str
+        }
+        es.index(index='network_traffic', body=document)
