3f92d5c901a0182c821da19fe71d7f294f36f20c
Bryan Rochadel <xadrez90@outlook.com>
2024-08-01 20:23:06 -0300

    time

--- captureLogs.py
+++ captureLogs.py
@@ -1,8 +1,12 @@
 import logging
-from scapy.all import sniff, IP, Ether, DNS, DNSQR, DNSRR
+import time
+from scapy.all import sniff, IP, Ether, DNS, DNSQR
+from threading import Thread
 
 # Configurar o logging
 logging.basicConfig(filename='network_logs.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+packet_log = []  # Lista para armazenar os logs temporários
 
 def packet_handler(packet):
     log_entry = []
@@ -29,6 +33,19 @@ def packet_handler(packet):
 
     if log_entry:
         logging.info(", ".join(log_entry))
+        packet_log.append(", ".join(log_entry))
 
-# Capturar pacotes
-sniff(prn=packet_handler, store=0, iface='eth0', promisc=True)
+def print_logs_periodically():
+    while True:
+        if packet_log:
+            for entry in packet_log:
+                print(entry)
+            packet_log.clear()  # Limpar a lista após imprimir
+        time.sleep(5)
+
+# Iniciar a captura de pacotes em um thread separado
+sniff_thread = Thread(target=lambda: sniff(prn=packet_handler, store=0, iface='eth0', promisc=True))
+sniff_thread.start()
+
+# Iniciar o loop de impressão periódica
+print_logs_periodically()
